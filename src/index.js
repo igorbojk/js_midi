b76bcae0468924158e8829cb91ad3d57ce80bf07
igorbojk <igorbojk@gmail.com>
2021-11-14 21:28:31 +0200

    feat: WIP

--- src/index.js
+++ src/index.js
@@ -4,17 +4,29 @@ import { midiNotes } from './const/midiNotes';
 const notesField = document.getElementById('notes');
 const bpmField = document.getElementById('bpm');
 const releaseField = document.getElementById('release');
-
+const attackField = document.getElementById('attack');
+const decayField = document.getElementById('decay');
+const sustainField = document.getElementById('sustain');
+const startBtn = document.getElementById('start');
+const stopBtn = document.getElementById('stop');
+const suspendBtn = document.getElementById('suspend');
+const eps = 0.01;
+const threshold = 0.001;
 
 let ctx;
-const threshold = 0.001;
 let releaseValue = 1;
-const eps = 0.01;
+let attackValue = 0;
+let decayValue = 0;
+let sustainValue = 50;
+let instrumentType = 'default';
+let type = 'square';
 let parserError = false;
 let sources = [];
+let timer = null;
 
 function start() {
-    ctx = new AudioContext() || new webkitAudioCondext()
+    clearTimeout(timer);
+    ctx = new AudioContext() || new webkitAudioCondext();
     if (!notesField.value || !bpmField.value) {
         return;
     }
@@ -27,9 +39,9 @@ function start() {
         return;
     }
     notes.forEach((note, index) => {
-        const freq = note.number !== 0 ? Math.pow(2, (note.number - 69) / 12) * 440 : 0;
+        let freq = note.number !== 0 ? Math.pow(2, (note.number - 69) / 12) * 440 : 0;
         const noteTime = note.isLong ? note.time * 1.5 : note.time;
-        playNote(freq, time, (60 / bpmField.value) * noteTime, index);
+        playNote(freq, time, (60 / bpmField.value) * noteTime, index, index === notes.length - 1);
         time += (60 / bpmField.value) * noteTime;
     });
 
@@ -54,51 +66,156 @@ function parseNotes(notes) {
     return arr;
 };
 
-function playNote(freq, startTime = 0, endTime, index) {
+function playNote(freq, startTime = 0, endTime, index, isLast) {
     const osc = ctx.createOscillator();
-    sources[index] = osc;
-    osc.type = 'square';
-    osc.frequency.value = freq;
-
     const release = ctx.createGain();
-    osc.connect(release);
-    release.connect(ctx.destination);
+    const attack = ctx.createGain();
+    const decay = ctx.createGain();
+
+    sources[index] = osc;
+    osc.type = type;
+    osc.frequency.value = freq;
+    osc.connect(attack);
+
+    let time = startTime + threshold;
+    if (attackValue > threshold) {
+        time += attackValue;
+    }
+    attack.gain.setValueAtTime(0.00001, startTime);
+    attack.gain.exponentialRampToValueAtTime(0.9, time);
+    attack.connect(decay);
+
+    decay.gain.setValueAtTime(1, startTime + attackValue);
+    decay.gain.exponentialRampToValueAtTime(
+        sustainValue / 100,
+        startTime + attackValue + decayValue
+    );
+    decay.connect(release)
+
+    const stopTime = startTime + endTime + Math.max(releaseValue, threshold);
+
     release.gain.setValueAtTime(0.9, startTime);
     release.gain.exponentialRampToValueAtTime(
         0.00001,
-        startTime + endTime + Math.max(releaseValue, threshold)
+        stopTime
     );
+
+    release.connect(ctx.destination);
     osc.start(startTime);
-    osc.stop(startTime + endTime + Math.max(releaseValue, threshold));
+    osc.stop(stopTime);
+    if (isLast) {
+        timer = setTimeout(() => {
+            stop();
+        }, stopTime * 1000)
+    }
 };
 
 function stopAll() {
     sources.forEach(el => el.stop());
     sources = [];
+};
+
+function stop() {
+    ctx.close();
+    stopBtn.setAttribute('disabled', 'disabled');
+    suspendBtn.setAttribute('disabled', 'disabled');
+    suspendBtn.textContent = 'pause';
 }
 
-document.addEventListener('submit', function(evt) {
+function setValuesToFields() {
+    switch (instrumentType) {
+        case 'type1':
+            {
+                type = 'square';
+                releaseValue = 1;
+                attackValue = 0;
+                decayValue = 0;
+                sustainValue = 50;
+                break
+            }
+        case 'type2':
+            {
+                type = 'triangle';
+                releaseValue = 4;
+                decayValue = 1.4;
+                attackValue = 0.05;
+                sustainValue = 50;
+                break
+            }
+        case 'type3':
+            {
+                type = 'sine';
+                releaseValue = 3.6;
+                attackValue = 0.05;
+                decayValue = 1.1;
+                sustainValue = 22;
+                break
+            }
+        case 'type4':
+            {
+                type = 'sine';
+                releaseValue = 5.1;
+                attackValue = 0.4;
+                decayValue = 1.9;
+                sustainValue = 50;
+                break
+            }
+    }
+}
+
+startBtn.addEventListener('click', function(evt) {
     evt.stopImmediatePropagation();
-    evt.preventDefault();
     start();
+    stopBtn.removeAttribute('disabled');
+    suspendBtn.removeAttribute('disabled');
 }, true);
 
-document.getElementById('stop').addEventListener('click', function(evt) {
+stopBtn.addEventListener('click', function(evt) {
     evt.stopImmediatePropagation();
-    ctx.close();
+    clearTimeout(timer);
+    stop();
 }, true);
 
-document.getElementById('pause').addEventListener('click', function(evt) {
+suspendBtn.addEventListener('click', function(evt) {
     evt.stopImmediatePropagation();
-    ctx.suspend();
+    clearTimeout(timer);
+    if (ctx.state === 'running') {
+        ctx.suspend();
+        suspendBtn.textContent = 'resume';
+    } else {
+        ctx.resume();
+        suspendBtn.textContent = 'pause';
+    }
 }, true);
 
-document.getElementById('resume').addEventListener('click', function(evt) {
+releaseField.addEventListener('mouseup', function(evt) {
     evt.stopImmediatePropagation();
-    ctx.resume();
+    releaseValue = +releaseField.value;
 }, true);
 
-document.getElementById('release').addEventListener('mouseup', function(evt) {
+attackField.addEventListener('mouseup', function(evt) {
     evt.stopImmediatePropagation();
-    releaseValue = releaseField.value;
+    attackValue = +attackField.value;
 }, true);
+
+decayField.addEventListener('mouseup', function(evt) {
+    evt.stopImmediatePropagation();
+    decayValue = +decayField.value;
+}, true);
+
+sustainField.addEventListener('mouseup', function(evt) {
+    evt.stopImmediatePropagation();
+    sustainValue = +sustainField.value;
+}, true);
+
+sustainField.addEventListener('mouseup', function(evt) {
+    evt.stopImmediatePropagation();
+    sustainValue = +sustainField.value;
+}, true);
+
+document.getElementsByName('instrument').forEach(el => {
+    el.addEventListener('change', () => {
+        instrumentType = el.value;
+        setValuesToFields();
+    });
+})
